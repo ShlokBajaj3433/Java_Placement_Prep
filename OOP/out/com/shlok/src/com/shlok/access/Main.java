649695d7368c7ae26823317e2c8b7ceefaa1cddb
Shlok Bajaj <shlokpbajaj000@gmail.com>
2026-04-11 18:28:59 +0530

    Objects in Java

--- OOP/out/com/shlok/src/com/shlok/access/Main.java
+++ OOP/out/com/shlok/src/com/shlok/access/Main.java
@@ -3,6 +3,8 @@ package com.shlok.access;
 public class Main {
     public static void main(String[] args) {
         A obj = new A(5,"Shlok");
-    
+
+        System.out.println(obj instanceof A);
+        System.out.println(obj instanceof Subclass);
     }
 }
--- OOP/out/com/shlok/src/com/shlok/access/Object.java
+++ OOP/out/com/shlok/src/com/shlok/access/Object.java
@@ -1,4 +1,42 @@
 package com.shlok.access;
 
 public class Object {
+    public Object() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(java.lang.Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected java.lang.Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main() {
+            Object obj1 = new Object();
+            Object obj2 = new Object();
+
+            System.out.println("HashCode of obj1: " + obj1.hashCode());
+            System.out.println("HashCode of obj2: " + obj2.hashCode());
+
+            System.out.println("obj1 equals obj2: " + obj1.equals(obj2));
+
+            System.out.println("String representation of obj1: " + obj1.toString());
+
+            System.out.println(obj1.getClass().getName());
+    }
+
 }
