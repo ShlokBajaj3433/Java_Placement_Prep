fd856aeb856ea750cd889e9dc8c13aee8c20d29a
Shlok Bajaj <shlokpbajaj000@gmail.com>
2026-06-05 17:14:22 +0530

    feat(array): add Average, Demo, Max, RemoveDuplicate, and Search classes for array operations

---
+++ OOP/out/com/shlok/src/Basic/Array/Average.java
@@ -0,0 +1,12 @@
+package Basic.Array;
+
+public class Average {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Average is " + (sum/arr.length));  
+    }
+}
---
+++ OOP/out/com/shlok/src/Basic/Array/Demo.java
@@ -0,0 +1,34 @@
+package Basic.Array;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,4,4,56,7,4};
+        int arr2[] = new int[10];
+
+        arr2[0] = 1;
+        arr2[1] = 2;
+        arr2[2] = 3;
+        arr2[3] = 4;
+        arr2[4] = 5;
+
+        System.out.println(arr2[1]);
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        
+        Scanner sc = new Scanner(System.in);
+        for (int i = 5; i < arr2.length; i++) {
+            System.out.println("Enter the value for index " + i);
+            arr2[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+
+    }
+}
